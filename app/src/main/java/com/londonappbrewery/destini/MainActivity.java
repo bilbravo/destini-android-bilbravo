e785c83e30fab22f6d6df614cfa75c61a1fe552b
bilbravo <bilbravo@gmail.com>
2020-01-18 07:56:26 -0500

    updated game logic and endings

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,29 +2,70 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
-
+    TextView mStoryTextView;
+    Button mBottomButton;
+    Button mTopButton;
+    int mStoryIndex = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
-
+        // find the views
+        mStoryTextView = findViewById(R.id.storyTextView);
+        mBottomButton = findViewById(R.id.buttonBottom);
+        mTopButton = findViewById(R.id.buttonTop);
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+        mTopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                if( mStoryIndex == 1 || mStoryIndex == 2) {
+                    mStoryIndex = 3;
+                    mStoryTextView.setText(R.string.T3_Story);
+                    mTopButton.setText(R.string.T3_Ans1);
+                    mBottomButton.setText(R.string.T3_Ans2);
+                } else if ( mStoryIndex == 3 ) {
+                    mStoryTextView.setText(R.string.T6_End);
+
+                }
+
+
+            }
+        });
 
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
 
+        mBottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if( mStoryIndex == 1 ) {
+                    mStoryIndex = 2;
+                    mStoryTextView.setText(R.string.T2_Story);
+                    mTopButton.setText(R.string.T2_Ans1);
+                    mBottomButton.setText(R.string.T2_Ans2);
+                } else if (mStoryIndex == 2 ) {
+                    mStoryTextView.setText(R.string.T4_End);
+                } else if (mStoryIndex == 3 ) {
+                    mStoryTextView.setText(R.string.T5_End);
+                }
+
+
+            }
+        });
 
     }
 }
